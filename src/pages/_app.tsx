68dc1199c6e9b61d05308d26d485dd1437723938
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-06 23:33:51 +0000

    adds global styles and page syle example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import GlobalStyles from "../styles/GlobalStyles";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 };
 
 
