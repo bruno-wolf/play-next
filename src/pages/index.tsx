764c0253f4adec77398919b59ab7dcfe9e33faf5
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-07 15:26:48 +0000

    lists products using client side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect( () => {
+    fetch('http://localhost:3333/recommended')
+      .then(response => {
+        response.json().then( data => {
+          setRecommendedProducts(data);
+        })
+      })
+  }, []);
+
   return (
-    <Title>Hello Wolf</Title>
+
+    <section>
+      <Title>Products</Title>
+      <ul>
+        {recommendedProducts.map(product => (
+          <li key={product.id}>
+            {product.title}
+          </li>
+        ))}
+      </ul>
+    </section>
   )
 }
