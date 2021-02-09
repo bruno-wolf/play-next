403b1ef36c2aed7b0430ac402530ed19acd1018f
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-09 22:19:12 +0000

    adds lazy load to modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,7 @@
+export default function Modal () {
+  return (
+    <div>
+      Do you really want to add this item to cart?
+    </div>
+  )
+}
--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,9 +1,27 @@
 import { useRouter } from 'next/router';
+import { useCallback, useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const Modal = dynamic(
+  () => import('../../../components/modal'),
+  { loading: () => <p>Loading... </p>, ssr: false }
+)
 
 export default function Product() {
   const router = useRouter();
+  const [ modalVisibility, setModalVisibility ] = useState(false);
+
+  const handleAddToCart = useCallback(() => {
+    setModalVisibility(!modalVisibility);
+  }, [modalVisibility]);
 
   return (
-    <h1>{router.query.slug}</h1>
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      { modalVisibility && <Modal />}
+    </div>
   )
 }
